ea6d09341dc66a6a51e72315243bad4f8b550924
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-06-21 16:34:39 +0530

    Added Modal via React Portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div>
       <Fragment>
+        <Cart/>
         <Header />
         <main>
           <Meals />
