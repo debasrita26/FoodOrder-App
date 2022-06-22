1c49559c252e9b358466ba1605aea6a90650d0cd
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-06-22 14:58:08 +0530

    Removing items from the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,9 +9,13 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const totalamount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
