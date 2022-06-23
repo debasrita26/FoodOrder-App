6de829adf0a92cee8368acbdeff2321026838ff1
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-06-23 21:57:38 +0530

    Sumitted and stored the cart data in the firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -24,6 +24,16 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://foodorder-app-e6b79-default-rtdb.firebaseio.com/orders.json',{
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
+  };
+
   const totalamount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -61,7 +71,9 @@ const Cart = (props) => {
         <span>{totalamount}</span>
       </div>
       {/* rendered the checkout form below the total amount */}
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
